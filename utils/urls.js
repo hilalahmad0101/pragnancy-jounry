751aafcd1c0a34b5fb63e14f2332a84dfe646aca
hilalahmad32 <programmerhero6@gmail.com>
2023-04-29 00:15:50 +0500

    add url

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 export const mainUrl = "https://preg-api.medicdn.com";
-export const loginUrl = `${mainUrl}/login/`;
+export const loginUrl = `${mainUrl}/user/api/v1/user/login/`;
 export const getAllBlogCategoryUrl = `${mainUrl}/blog_category/api/v1/all/`;
 export const getCreateBlogCategoryUrl = `${mainUrl}/blog_category/api/v1/create`;
 export const getDeleteBlogCategoryUrl = `${mainUrl}/blog_category/api/v1/delete`;
